ffd94eda3f4de2a1091c5a7991f56da95a170b04
Sylvain Ferlac <sylvain@cirdan.net>
2019-09-25 17:30:52 +0200

    Utilisation des gestes dans le jeu

--- src/Chifoumi.php
+++ src/Chifoumi.php
@@ -10,20 +10,14 @@ class Chifoumi
     const GESTE_PIERRE = "pierre";
     const GESTE_CISEAUX = "ciseaux";
 
-    public function joue($gesteJoueur1, $gesteJoueur2)
+    public function joue(Geste $gesteJoueur1, Geste $gesteJoueur2)
     {
 
-        if($gesteJoueur1==self::GESTE_FEUILLE && $gesteJoueur2== self::GESTE_PIERRE){
-            return self::GESTE_FEUILLE;
+        if($gesteJoueur1->gagneContre($gesteJoueur2)){
+            return $gesteJoueur1->intitule();
         }
-        if($gesteJoueur1==self::GESTE_FEUILLE && $gesteJoueur2== self::GESTE_CISEAUX){
-            return self::GESTE_CISEAUX;
-        }
-        if($gesteJoueur1==self::GESTE_PIERRE && $gesteJoueur2== self::GESTE_CISEAUX){
-            return self::GESTE_PIERRE;
-        }
-        if($gesteJoueur1==self::GESTE_CISEAUX && $gesteJoueur2== self::GESTE_FEUILLE){
-            return self::GESTE_CISEAUX;
+        if($gesteJoueur2->gagneContre($gesteJoueur1)){
+            return $gesteJoueur2->intitule();
         }
         return self::RESULTAT_EGALITE;
     }
--- test/ChifoumiTest.php
+++ test/ChifoumiTest.php
@@ -12,27 +12,27 @@ class ChifoumiTest extends TestCase
         $partie = new Chifoumi();
         $this->assertEquals(
             Chifoumi::RESULTAT_EGALITE,
-            $partie->joue(Chifoumi::GESTE_FEUILLE, Chifoumi::GESTE_FEUILLE)
+            $partie->joue(new Feuille(), new Feuille())
         );
     }
     public function test_feuille_contre_pierre_gagne(){
         $partie=new Chifoumi();
-        $resultat=$partie->joue(Chifoumi::GESTE_FEUILLE, Chifoumi::GESTE_PIERRE);
-        $this->assertEquals($resultat,Chifoumi::GESTE_FEUILLE);
+        $resultat=$partie->joue(new Feuille(), new Pierre());
+        $this->assertEquals($resultat,Feuille::GESTE);
     }
     public function test_feuille_contre_ciseaux_perd(){
         $partie=new Chifoumi();
-        $resultat=$partie->joue(Chifoumi::GESTE_FEUILLE, Chifoumi::GESTE_CISEAUX);
-        $this->assertEquals($resultat,Chifoumi::GESTE_CISEAUX);
+        $resultat=$partie->joue(new Feuille(), new Ciseaux());
+        $this->assertEquals($resultat,Ciseaux::GESTE);
     }
     public function test_pierre_contre_ciseaux_gagne(){
         $partie=new Chifoumi();
-        $resultat=$partie->joue(Chifoumi::GESTE_PIERRE, Chifoumi::GESTE_CISEAUX);
-        $this->assertEquals($resultat,Chifoumi::GESTE_PIERRE);
+        $resultat=$partie->joue(new Pierre(), new Ciseaux());
+        $this->assertEquals($resultat,Pierre::GESTE);
     }
     public function test_ciseaux_contre_feuille_gagne(){
         $partie=new Chifoumi();
-        $resultat=$partie->joue(Chifoumi::GESTE_CISEAUX, Chifoumi::GESTE_FEUILLE);
-        $this->assertEquals($resultat,Chifoumi::GESTE_CISEAUX);
+        $resultat=$partie->joue(new Ciseaux(), new Feuille());
+        $this->assertEquals($resultat,Ciseaux::GESTE);
     }
 }
